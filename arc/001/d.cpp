f11605f307f6e857d2a182684df38eb4e57d58ae
Kugayama Nana <nonamea.774@gmail.com>
2018-02-18 14:24:44 +0900

    TLEいっぱいだけど近づいた。

--- arc/001/d.cpp
+++ arc/001/d.cpp
@@ -5,7 +5,7 @@
 #include<algorithm>
 #include<array>
 #include<tuple>
-#include <iomanip>
+#include<iomanip>
 #include<cmath>
 
 using std::begin;
@@ -15,7 +15,7 @@ double seppen(int s, int e, int n, int i) {
   return s + static_cast<double>(e - s) * i / n;
 }
 
-double len(int s, int top, int g, int tail) {
+double len(long s, long top, long g, long tail) {
   return std::pow((s-g)*(s-g) + (tail-top)*(tail-top), 0.5);
 }
 
@@ -27,18 +27,27 @@ double solve(std::vector<std::pair<int, int>> const& v, int start, int goal, int
   int mygoal(goal);
   while(!found) {
     bool fail(false);
-    for(int i(top + 1); i < mytail; ++i) {
+    int i(mytail - 1);
+    double s;
+    for(; i > top; --i) {
       auto w = v[i];
-      double s = seppen(start, mygoal, mytail - 1, i);
+      s = seppen(start, mygoal, mytail, i);
       if(!(w.first <= s && s <= w.second)) {
-        mytail = i;
-        mygoal = (s < w.first) ?
-          w.first :
-          w.second;
         fail = true;
+        break;
       }
     }
-    if(!fail) found = true;
+    if(fail) {
+      mytail = i;
+      auto w = v[mytail];
+      mygoal = (s < w.first) ?
+        w.first :
+        w.second;
+    }
+    else found = true;
+    if(mytail == top) {
+      exit(-1);
+    }
   }
   double current = len(start, top, mygoal, mytail);
   return current + solve(v, mygoal, goal, mytail, tail);
@@ -48,11 +57,11 @@ int main(int, char**) {
   int n;
   int start, goal;
   std::cin >> n >> start >> goal;
-  std::vector<std::pair<int, int>> v;
+  std::vector<std::pair<int, int>> v(n + 1);
   for(int i(0); i < n + 1; ++i) {
     int l, r;
     std::cin >> l >> r;
-    v.push_back(std::make_pair(l, r));
+    v[i] = std::make_pair(l, r);
   }
 
   std::cout << std::setprecision(16) << solve(v, start, goal, 0, n) << std::endl;
